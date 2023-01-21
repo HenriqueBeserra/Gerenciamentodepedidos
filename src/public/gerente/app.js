b8c019cdcf44fd8fa2ca3eaf69c227d2376a4be0
HenriqueBeserra <millerhiquinho@gmail.com>
2023-01-21 20:04:31 -0300

    Criando modal de pedidos, form

--- src/public/gerente/app.js
+++ src/public/gerente/app.js
@@ -1,11 +1,11 @@
 const $criarPedidos = document.querySelector("#criarPedido")
 const $modal = document.querySelector(".modal")
 let main = document.querySelector (".main-content")
-console.log($criarPedidos)
+const pedidos = document.querySelector(".headerPedidos")
+const botaocriar = document.querySelector("#criar")
 
 async function  openModal (edit = false, index = 0) {
-   await $modal.classList.toggle("active")
-   await main.classList.toggle("active2")
+    await $modal.classList.toggle("active")
     
     if($modal.classList.contains('active')) {
         console.log('ativo')
@@ -13,11 +13,33 @@ async function  openModal (edit = false, index = 0) {
         main.addEventListener("click", () => {
             console.log("mouse saiu")
             $modal.classList.remove("active")
-            main.classList.remove("active2")
+            
         })
     }
 
 } 
+
+ botaocriar.addEventListener("click", function adicionaPedido(infosDoEvento) {
+    
+    infosDoEvento.preventDefault();
+    console.log("Criado")    
+
+
+    const list = document.createElement("ul")
+    list.innerHTML = `<li>001</li>
+                     <li>Servir</li>
+                     <li>100</li>
+                     <li>22.02.2023</li>
+                     <li>Valtemir</li>
+                     <li>Ponte</li>
+                     <li>900</li> `
+    list.classList.add("pedidosList") 
+    pedidos.appendChild(list)
+
+})
+
+
+
 
 
 
